46c3ba5ad814d02cfd64392b1f17c9cdcbd70308
vpkumara <vpkumara@gmail.com>
2015-08-23 23:53:43 +0800

    second comit

--- cl-project1/src/main/java/com/pathma/ci/project1/App.java
+++ cl-project1/src/main/java/com/pathma/ci/project1/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("I am commiting to check deploy");
+        System.out.println("I am commiting to check deploy 2nd deploy");
     }
 }
