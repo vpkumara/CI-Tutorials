f7b0e393c054c6bc8a737f51b5b05b5b618a065b
vpkumara <vpkumara@gmail.com>
2015-08-24 00:44:31 +0800

    sadsdasd

--- cl-project1/src/main/java/com/pathma/ci/project1/App.java
+++ cl-project1/src/main/java/com/pathma/ci/project1/App.java
@@ -9,6 +9,7 @@ public class App
 	
 	public void addNewMethod(){
 		System.out.println( "Hello World!" );
+		System.out.println( "Hello World!" );
 	}
     public static void main( String[] args )
     {
